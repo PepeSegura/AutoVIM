da22c03a2f230b1b32d6aca2c70bd4ff3ed054b4
Pepe Segura Carreno <psegura-@c1r17s6.42madrid.com>
2022-08-27 20:52:13 +0200

    AutoVIM V1

---
+++ AutoVIM.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#Clone repo and enter in folder
+git clone git@github.com:PepeSegura/AutoVIM.git
+sleep 2
+cd $(pwd)/AutoVIM
+
+#Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Backup zshrc and vimrc files
+mkdir -p ~/backup_files
+cp ~/.vimrc ~/backup_files/.vimrc_backup
+cp ~/.zshrc ~/backup_files/.zsh_backup
+
+#Copy configurated files to each place
+mkdir -p ~/.vim/colors
+cp $(pwd)/source/sublimemonokai.vim ~/.vim/colors/
+cp $(pwd)/source/.vimrc ~/
+cp $(pwd)/source/.zshrc ~/
+source ~/.zshrc
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/AutoVIM.sh)"
--- prueba.sh
+++
@@ -1,5 +0,0 @@
-git clone git@github.com:PepeSegura/AutoVIM.git
-sleep 3
-cd $(pwd)/AutoVIM
-ls
-echo "HOLA EL SCRIP FUNCIONA"
