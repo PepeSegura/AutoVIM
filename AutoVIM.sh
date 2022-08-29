c3b22d89d39532845330188e93baa5fdfb9c5c24
psegura- <37574170+PepeSegura@users.noreply.github.com>
2022-08-29 21:20:45 +0200

    AutoVIM V3

--- AutoVIM.sh
+++ AutoVIM.sh
@@ -1,27 +1,28 @@
 #!/bin/zsh
 
-#Clone repo and enter in folder
-git clone -q https://github.com/PepeSegura/AutoVIM.git
-sleep 2
-cd $(pwd)/AutoVIM
-echo "Repositorio Clonado"
-
 #Install Oh My Zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -e ~/.oh-my-zsh ]
+then
+    echo "Oh My Zsh is already Installed"
+else
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 #Backup zshrc and vimrc files
 mkdir -p ~/backup_files
 cp ~/.vimrc ~/backup_files/.vimrc_backup
 cp ~/.zshrc ~/backup_files/.zsh_backup
 
-#Copy configurated files to each place
+#Create folder for VIM Themes
 mkdir -p ~/.vim/colors
-#echo "$(curl -fsSL https://raw.githubusercontent.com/erichdongubler/vim-sublime-monokai/master/colors/sublimemonokai.vim)" > ~/.vim/colors/sublimemonokai.vim
-cp $(pwd)/source/sublimemonokai.vim ~/.vim/colors/
-cp $(pwd)/source/.vimrc ~/
-cp $(pwd)/source/.zshrc ~/
+
+#Sublime Monokai
+echo "$(curl -fsSL https://raw.githubusercontent.com/erichdongubler/vim-sublime-monokai/master/colors/sublimemonokai.vim)" > ~/.vim/colors/sublimemonokai.vim
+
+#.VIMRC
+echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.vimrc)" > ~/.vimrc
+
+#Apply the zshrc changes
 source ~/.zshrc
 echo "Abre VIM para ver los cambios 🥰"
-rm -rf $(pwd)/AutoVIM
 
-#zsh -c "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/AutoVIM.sh)"
