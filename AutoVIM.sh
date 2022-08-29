780183ae7b2441ed895cb23730616ebb15b4c2e4
psegura- <37574170+PepeSegura@users.noreply.github.com>
2022-08-29 22:15:00 +0200

    AutoVIM V3

--- AutoVIM.sh
+++ AutoVIM.sh
@@ -4,25 +4,69 @@
 if [ -e ~/.oh-my-zsh ]
 then
     echo "Oh My Zsh is already Installed"
+    sleep 1
 else
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-#Backup zshrc and vimrc files
-mkdir -p ~/backup_files
-cp ~/.vimrc ~/backup_files/.vimrc_backup
-cp ~/.zshrc ~/backup_files/.zsh_backup
 
-#Create folder for VIM Themes
-mkdir -p ~/.vim/colors
+#Configure .zshrc
+if [[ -e ~/.zshrc && ~/backup_files]]
+then
+    cp ~/.zshrc ~/backup_files/.zsh_backup
+    echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.zshrc)" > ~/.zshrc
+else
+    mkdir -p ~/backup_files
+    cp ~/.zshrc ~/backup_files/.zsh_backup
+    echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.zshrc)" > ~/.zshrc
+fi
 
-#Sublime Monokai
-echo "$(curl -fsSL https://raw.githubusercontent.com/erichdongubler/vim-sublime-monokai/master/colors/sublimemonokai.vim)" > ~/.vim/colors/sublimemonokai.vim
 
-#.VIMRC
-echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.vimrc)" > ~/.vimrc
+#Configure .vimrc
+if [[ -e ~/.vimrc $$ && ~/backup_files]]
+then
+    cp ~/.vimrc ~/backup_files/.vimrc_backup
+    echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.vimrc)" > ~/.vimrc
+else
+    mkdir -p ~/backup_files
+    cp ~/.vimrc ~/backup_files/.vimrc_backup
+    echo "$(curl -fsSL https://raw.githubusercontent.com/PepeSegura/AutoVIM/master/source/.vimrc)" > ~/.vimrc
+fi
 
-#Apply the zshrc changes
+
+#Configure Sublime Monokai
+if [ -e ~/.vim/colors/sublimemonokai.vim]
+then
+    echo "You already have The Best theme ever!!!"
+    sleep 3
+else
+    mkdir -p ~/.vim/colors
+    echo "$(curl -fsSL https://raw.githubusercontent.com/erichdongubler/vim-sublime-monokai/master/colors/sublimemonokai.vim)" > ~/.vim/colors/sublimemonokai.vim
+fi
+
+
+#Apply the changes
 source ~/.zshrc
-echo "Abre VIM para ver los cambios 🥰"
+
+echo "                                                                                                                                                
+                                    ///***///////*                              
+                            #(#***/,,,,,,,,#.,,,,,,*/                           
+                           /**,,,,,,,,.(#######,,,,,,,,/                        
+                          //,,,,,,,.#####.(/.########%.**                       
+                          *,......#####.@@@.,@########%..*                      
+                         *,...../######(@@@..@######,@*&.*                      
+                        #...########################&.@*,                       
+                         ..#############,###########&@@%/                       
+                         *..#,##########################&                       
+                           ,...,##############(######/##%                       
+   PSEGURA-                 ...##################(###(#                        
+   PATROCINA                  /#####(**#########((((((.                         
+   TU                      *%%%%%%#####/###*#####*                              
+   NUEVO                 ,%%%%%%%%#####%#(##(%&                                 
+   VIM                  *%%%%%############%%%%%#/(                              
+                        %%%%%##(###########%%%%%%%*%&(                          
+                       /#%%%%##%#(######(*%@&(%%#%/###(/                        
+                       #%%%######((/@@@@@@@@@@*@#%(#,*#&                        
+                      ,#%%###(####(@@@@@@@@@@@#@@@@@&(%                         
+                       *&#((######/@@@@@@@@@@@&@@@@@#*                          "
 
--- source/.zshrc
+++ source/.zshrc
@@ -100,6 +100,20 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/sgoinfre/students/psegura-/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/sgoinfre/students/psegura-/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/sgoinfre/students/psegura-/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/sgoinfre/students/psegura-/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 
 #Alias
 alias gme="bash ./libft-war-machine/grademe.sh"
@@ -119,20 +133,3 @@ source $HOME/.brewconfig.zsh
 
 # Export
 export PATH=$HOME/.brew/bin:$PATH
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/sgoinfre/students/psegura-/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/sgoinfre/students/psegura-/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/sgoinfre/students/psegura-/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/sgoinfre/students/psegura-/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
